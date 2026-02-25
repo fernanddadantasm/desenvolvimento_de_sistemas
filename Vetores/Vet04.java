bf21a710c92d44d6d320aee888cde9209cbf9dcf
Aluno <Aluno@C302-LAB-000.senaiba.com.br>
2026-02-25 14:41:06 -0300

    Vetores

---
+++ Vetores/Vet04.java
@@ -0,0 +1,17 @@
+// package Vetores;
+
+// public class Vet04 {
+//     public static void main(String[] args) {
+//         nome[0]="Fernanda";
+//         nome[1]="Maria";
+
+//         for (int i = 0; i < nome.length; i++) {
+//             System.out.println("Posição "+i+" = "+nome[i]);
+            
+            
+//         }
+//             System.out.println(nome[1]);
+
+//             for (String n: nome) {
+//                 System.out.println(n);
+//             }
---
+++ Vetores/Vet05.java
@@ -0,0 +1,24 @@
+package Vetores;
+
+import java.util.Scanner;
+
+public class Vet05 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        String [] nomes = new String[3];
+
+        for(int i=0;i<nomes.length;i++){
+            System.out.println("Digite o "+i+"º nome:");
+            nomes[i] = sc.nextLine();
+
+            System.out.println("O nome é: "+nomes[i]);
+        }
+        for(String nome: nomes){
+            System.out.println(nome);
+        }
+        sc.close();
+    }
+
+}
